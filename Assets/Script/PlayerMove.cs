589688dd316d2f246034da718e4ce26aa623a2c1
hammon737 <hammon737@morrisville.edu>
2026-09-02 12:55:41 -0400

    .

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float movespeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Test the Star function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
