2c852911522731f8adbf8c4d96c17dd111e2d960
hammon737 <hammon737@morrisville.edu>
2026-09-02 13:53:58 -0400

    Movement Update
    Added simple movement script

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] float movespeed = 5f;
+    [SerializeField] float movespeed = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Test the Star function");
+        Debug.Log("Movement script started");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(movespeed * Time.deltaTime, 0f, 0f);
     }
 }
